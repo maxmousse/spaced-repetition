c7c5c05a381e584c90fddf8804cc2457854bcce7
Maxim Darré <maxim.darre@gmail.com>
2024-04-03 17:02:33 +0200

    feat: setup graphql codegen

--- apps/pwa/src/app/app.component.ts
+++ apps/pwa/src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Apollo, gql } from 'apollo-angular';
 import { map } from 'rxjs';
-
-const GET_USERS = gql`
-  query GetUsers {
-    getUsers {
-      id
-      firstName
-      lastName
-      email
-    }
-  }
-`;
+import { GetUsersQuery } from './get-users.graphql';
 
 @Component({
   standalone: true,
@@ -24,9 +13,7 @@ const GET_USERS = gql`
 })
 export class AppComponent {
   title = 'pwa';
-  users$ = this.graphql
-    .query({ query: GET_USERS })
-    .pipe(map((result) => result.data));
+  users$ = this.getUsersQuery.fetch().pipe(map((result) => result.data));
 
-  constructor(private graphql: Apollo) {}
+  constructor(private getUsersQuery: GetUsersQuery) {}
 }
