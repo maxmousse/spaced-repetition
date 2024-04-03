50293f353baf967adb6b805b9c168d73a8fcb028
Maxim Darré <maxim.darre@gmail.com>
2024-04-03 16:21:01 +0200

    feat: add graphql support to pwa

--- apps/pwa/src/app/app.component.ts
+++ apps/pwa/src/app/app.component.ts
@@ -1,14 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Apollo, gql } from 'apollo-angular';
+import { map } from 'rxjs';
+
+const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      firstName
+      lastName
+      email
+    }
+  }
+`;
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pwa';
+  users$ = this.graphql
+    .query({ query: GET_USERS })
+    .pipe(map((result) => result.data));
+
+  constructor(private graphql: Apollo) {}
 }
