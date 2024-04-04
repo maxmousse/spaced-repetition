d5ea0cdbb157a0fbaf5d210b75c99cd5032a92b9
Maxim Darré <maxim.darre@gmail.com>
2024-04-04 17:27:40 +0200

    feat: add getNotes query and createNote mutation

---
+++ apps/api/src/graphql/note/types.rs
@@ -0,0 +1,53 @@
+use async_graphql::{InputObject, SimpleObject, ID};
+use chrono::{DateTime, FixedOffset};
+use prisma_client::note;
+
+#[derive(SimpleObject)]
+pub struct Note {
+    pub id: ID,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: DateTime<FixedOffset>,
+    pub title: String,
+    pub content: String,
+}
+
+#[derive(InputObject)]
+pub struct CreateNoteInput {
+    pub title: String,
+    pub content: String,
+}
+
+#[derive(InputObject)]
+pub struct UpdateNoteInput {
+    pub title: Option<String>,
+    pub content: Option<String>,
+}
+
+#[derive(InputObject)]
+pub struct DeleteNote {
+    pub id: ID,
+}
+
+impl From<note::Data> for Note {
+    fn from(value: note::Data) -> Self {
+        Note {
+            id: value.id.into(),
+            created_at: value.created_at,
+            updated_at: value.updated_at,
+            title: value.title,
+            content: value.content,
+        }
+    }
+}
+
+impl From<&note::Data> for Note {
+    fn from(value: &note::Data) -> Self {
+        Note {
+            id: value.id.clone().into(),
+            created_at: value.created_at.clone(),
+            updated_at: value.updated_at.clone(),
+            title: value.title.clone(),
+            content: value.content.clone(),
+        }
+    }
+}
--- apps/api/src/graphql/user/types.rs
+++ apps/api/src/graphql/user/types.rs
@@ -1,32 +1,32 @@
-use async_graphql::SimpleObject;
+use async_graphql::{SimpleObject, ID};
 use prisma_client::user;
 
 #[derive(SimpleObject)]
 pub struct User {
-    pub id: String,
+    pub id: ID,
     pub first_name: String,
     pub last_name: String,
     pub email: String,
 }
 
 impl From<user::Data> for User {
-    fn from(user: user::Data) -> Self {
+    fn from(value: user::Data) -> Self {
         User {
-            id: user.id,
-            first_name: user.first_name,
-            last_name: user.last_name,
-            email: user.email,
+            id: value.id.into(),
+            first_name: value.first_name,
+            last_name: value.last_name,
+            email: value.email,
         }
     }
 }
 
 impl From<&user::Data> for User {
-    fn from(user: &user::Data) -> Self {
+    fn from(value: &user::Data) -> Self {
         User {
-            id: user.id.clone(),
-            first_name: user.first_name.clone(),
-            last_name: user.last_name.clone(),
-            email: user.email.clone(),
+            id: value.id.clone().into(),
+            first_name: value.first_name.clone(),
+            last_name: value.last_name.clone(),
+            email: value.email.clone(),
         }
     }
 }
